bb076cc6f949b62075ebec5b9c297498a419d056
AbhisekOmkar <67184718+AbhisekOmkar@users.noreply.github.com>
2023-11-16 18:46:03 +0000

    conversion operations

---
+++ Basic/conversionOperations.js
@@ -0,0 +1,24 @@
+let score = "Abhisek"
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let valueInNumber = Number (score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+//Note
+//"33" => 33
+//"33abc" => NaN
+// true => 1; false => 0 
+
+let isLoggedIn = "abhisek"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(someNumber)
+console.log(typeof someNumber)
