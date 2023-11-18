ce7fd1a57c8ad160320835ca2da5cf24a24f6ba0
AbhisekOmkar <67184718+AbhisekOmkar@users.noreply.github.com>
2023-11-18 13:22:39 +0000

    conversion and operations

--- Basic/conversionOperations.js
+++ Basic/conversionOperations.js
@@ -22,3 +22,35 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(someNumber)
 console.log(typeof someNumber)
+
+//****************************************** Operations ***********************************/
+
+let value = 3
+let negValue = -value 
+console.log(negValue)
+
+let str1 = "Abhisek"
+let str2 = " Prasad"
+
+let str3 = str1 + str2
+console.log(str3)
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+console.log(3+ 4 * 5 % 3);
+
+console.log(+true);
+console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
+
